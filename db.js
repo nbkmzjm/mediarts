c46b92cb445778c8e5c65740ea58911943d18fe3
Thien Ngo <ngokhanhthien@yahoo.com>
2015-12-09 16:21:40 -0700

    fixing db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
-if (evn==='production'){
+if (env === 'production'){
 
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
