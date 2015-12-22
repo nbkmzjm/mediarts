e80e33445b7826b8b125e60a82fac01839598f62
Thien Ngo <ngokhanhthien@yahoo.com>
2015-12-21 22:35:58 -0700

    add custome function

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
 
 var db = require('./db.js');
 
 var _ = require('underscore');
 
-var PORT = process.env.PORT||3000;
+var PORT = process.env.PORT || 3000;
 
-var todoNext=1;
-var todos = [
-{id:1, description: 'meet for lunch', done: false },
-{id:2, description: 'meet for tennis', done: false },
-{id:3, description: 'meet for monvie', done: false }
-];
+var todoNext = 1;
+var todos = [{
+	id: 1,
+	description: 'meet for lunch',
+	done: false
+}, {
+	id: 2,
+	description: 'meet for tennis',
+	done: false
+}, {
+	id: 3,
+	description: 'meet for monvie',
+	done: false
+}];
 var middeware = require('./middleware.js');
 
 app.use(middeware.logger);
@@ -21,124 +30,150 @@ app.use(middeware.logger);
 app.use(bodyParser.json());
 
 
-app.get('/about', middeware.requireAuthentication,function(req, res){
+app.get('/about', middeware.requireAuthentication, function(req, res) {
 	res.send('About us');
 });
 
-app.get('/todos', function(req, res){
+app.get('/todos', function(req, res) {
 	var query = req.query;
 	var where = {};
 
-	if (query.hasOwnProperty('completed') && query.completed === 'true'){
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	} else if (query.hasOwnProperty('completed') && query.completed === 'false'){
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
 
-	if (query.hasOwnProperty('q') && query.q.length>0){
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 
-		where.description = { $like: '%'+query.q+'%' };
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
 
-	db.todo.findAll({where: where}).then(function (todos){
-		
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
+
 		res.json(todos);
-		
-	}, function (e){
+
+	}, function(e) {
 		res.status(500).send;
 
 	});
 
 
 })
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/todos/:id', function(req, res){
+app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
 
-	db.todo.findById(todoId).then(function (todo){
-		if (!!todo){
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
 			res.json(todo.toJSON());
-		}else{
+		} else {
 			res.status(404).send();
 		}
-	}, function (e){
+	}, function(e) {
 		res.status(500).send();
 
 	});
-		
-	
+
+
 });
 
-app.post('/todos', function (req, res){
+app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function (todo){
+	db.todo.create(body).then(function(todo) {
 		res.json(todo);
-	}, function (e){
+	}, function(e) {
 		res.status(400).json(e);
 	});
 
-	
+
 });
 
-app.delete('/todos/:id', function (req, res){
+app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
-		where:{
-			id:todoId
+		where: {
+			id: todoId
 		}
 
-	}).then(function (rowsDeleted){
+	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
 			res.status(404).json({
 				error: 'No todo with id'
 			});
-		} else{
+		} else {
 			res.status(204).send();
 		}
-	}, function (){
+	}, function() {
 		res.status(500).send();
 	})
 })
 
-db.sequelize.sync().then(function(){
-	app.listen(PORT, function(){
-	console.log('Helllo Express server started on PORT '+ PORT);
-	});
 
-});
 
-app.put('/todos/:id', function(req, res){
+app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
 	var attr = {};
 
-	if (body.hasOwnProperty('completed')){
+	if (body.hasOwnProperty('completed')) {
 		attr.completed = body.completed
 	}
 
-	if (body.hasOwnProperty('description')){
+	if (body.hasOwnProperty('description')) {
 		attr.description = body.description
 	}
 
-	db.todo.findById(todoId).then(function (todo){
-		if (todo){
-			todo.update(attr).then(function(){
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			todo.update(attr).then(function() {
 				res.json(todo.toJSON());
-			}, function (e){
+			}, function(e) {
 				res.status(400).json(e);
 
 			});
 		} else {
 			res.status(404).send();
 		}
-	}, function(){
+	}, function() {
 		res.status(400).send();
 	});
 
-
-
 });
 
+app.post('/user', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+
+	db.user.create(body).then(function(user) {
+		res.json(user.toPublicJSON());
+	}, function(e) {
+		res.status(400).json(e);
+
+	});
+})
+
+
+app.post('/user/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function() {
+		res.status(401).send();
+	});
+});
+
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Helllo Express server started on PORT ' + PORT);
+	});
+
+});
