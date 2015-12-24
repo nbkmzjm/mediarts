46ec9b30fd0ab09abe5ab7c76767b4abf687a250
Thien Ngo <ngokhanhthien@yahoo.com>
2015-12-23 20:26:14 -0700

    logout

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+cryptojs = require('crypto-js')
 module.exports = function(db) {
 
 	return {
@@ -6,12 +7,22 @@ module.exports = function(db) {
 			var token = req.get('Auth');
 			console.log(token);
 
-			db.user.findByToken(token).then(function (user){
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function (tokenIns){
+				if (!tokenIns){
+					throw new Error('token is not found');
+				}
+				request.token = tokenIns;
+				return db.user.findByToken(token);
+			}).then(function (user){
 				req.user = user;
 				next();
 			}, function (e) {
 				console.log(e);
-				res.status(401).send();
+				res.status(401).json({error:e.toString()});
 			});
 
 		},
--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.post('/user', function(req, res) {
 
 	db.user.create(body).then(function(user) {
 		res.json(user.toPublicJSON());
-	}, function(e) {
+	}, function (e) {
 		res.status(400).json(e);
 
 	});
@@ -168,14 +168,30 @@ app.post('/user', function(req, res) {
 
 app.post('/user/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
-	db.user.authenticate(body).then(function(user) {
+	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication')
-		res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
-	}, function() {
-		res.status(401).send();
+		userInstance = user;
+		return db.token.create({
+			token: token
+		});
+		
+	}).then(function (tokenInstance) {
+		console.log("tookenInstance created");
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function (e) {
+		res.status(401).json({error:e.toString()});
 	});
 });
+
+app.delete('/user/logout', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function (){
+		res.status(500).send();
+	});
+
+});
+
 
 db.sequelize.sync({
 	force: true
