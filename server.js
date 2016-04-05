8333b4c27d4b4885015be9560db79b7643c6774b
nbkmzjm <ngokhanhthien@yahoo.com>
2016-04-05 12:28:19 -0700

    4-5

--- server.js
+++ server.js
@@ -474,7 +474,7 @@ app.delete('/user/logout', middleware.requireAuthentication, function(req, res) 
 
 
 db.sequelize.sync(
-	// {force: true}
+	{force: true}
 ).then(function() {
 	
 	http.listen(PORT, function() {
