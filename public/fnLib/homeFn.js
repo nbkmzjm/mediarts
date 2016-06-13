981837f1eb31cca55dedb7ad47300702140cb925
nbkmzjm <ngokhanhthien@yahoo.com>
2016-06-13 11:12:57 -0700

    6-13b

--- public/fnLib/homeFn.js
+++ public/fnLib/homeFn.js
@@ -79,8 +79,8 @@ function calendarPick(){
 				
 					li.addEventListener('click', function(){
 						// $("#calendar").blur()
-						// $('#eventActionTable').length>0 ? 
-						// $('#eventActionTable').remove():'';
+						$('#eventActionTable').length>0 ? 
+						$('#eventActionTable').remove():'';
 						$("#calendar").datepicker({
 							// beforeShow: function(){
 							// 	// alert('blco')
@@ -92,7 +92,7 @@ function calendarPick(){
 							
 						});
 
-						// dateCalender.focus()
+						dateCalender.focus()
 
 					})
 					
