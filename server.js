1e9ee00e1c873bf00fe8981aab234ddcfddb831e
nbkmzjm <ngokhanhthien@yahoo.com>
2016-08-08 11:11:05 -0700

    8-8a

--- server.js
+++ server.js
@@ -485,23 +485,11 @@ var user = require('./server/serverUser.js');
 app.use('/users', user);
 
 
-umzug.up().then(function (migrations) {
-	console.log(migrations)
-  // "migrations" will be an Array with the names of
-  // pending migrations.
-  	db.sequelize.sync(
-	{force: false}
-	).then(function() {
-		
-		http.listen(PORT, function() {
-			console.log('Helllo Express server started on PORT ' + PORT);
-		});
-
-	});
-});
-
-
-// db.sequelize.sync(
+// umzug.up().then(function (migrations) {
+// 	console.log(migrations)
+//   // "migrations" will be an Array with the names of
+//   // pending migrations.
+//   	db.sequelize.sync(
 // 	{force: false}
 // 	).then(function() {
 		
@@ -510,3 +498,15 @@ umzug.up().then(function (migrations) {
 // 		});
 
 // 	});
+// });
+
+
+db.sequelize.sync(
+	{force: false}
+	).then(function() {
+		
+		http.listen(PORT, function() {
+			console.log('Helllo Express server started on PORT ' + PORT);
+		});
+
+	});
